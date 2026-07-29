8bf9de8b311b3fbbdaffe230f300dd15b711491e
Aryan V Babu <aryanvbabu@gmail.com>
2026-07-29 12:36:12 +0530

    replace logo

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,8 +1,15 @@
+import Image from "next/image";
 export default function Navbar() {
   return (
     <nav className="fixed top-0 left-0 right-0 z-50 backdrop-blur-md bg-black/60 border-b border-zinc-800">
       <div className="max-w-7xl mx-auto px-6 py-4 flex justify-between items-center">
-      <h1 className="text-2xl font-bold">NexusQ</h1>
+      <Image
+  src="/logo.png"
+  alt="NexusQ Logo"
+  width={180}
+  height={60}
+  priority
+/>
 
       <ul className="flex gap-8">
         <li><a href="#home">Home</a></li>
